fa093cfb49c37c42b361f39f6b139db8a683b02e
mobileappscloud <doc92606@gmail.com>
2014-01-05 17:38:24 -0800

    Initial Commit

---
+++ Test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by iMac on 1/5/14.
+//  Copyright (c) 2014 Testflight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Test/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by iMac on 1/5/14.
+//  Copyright (c) 2014 Testflight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
